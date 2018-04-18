3a76d40d3d9b8f8e713d04e3479a6906153ca8f1
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-04-18 13:15:06 +0200

    Added the dashboard panel with Angular routes

---
+++ api/conn.php
@@ -0,0 +1,5 @@
+﻿<?php
+
+	//$conn = new PDO('mysql:host=127.0.0.1;dbname=ndusys0_makhawinidb','ndusys0_makhawin','xVeQFh8Rc0MfnMf');
+	$conn = new PDO('mysql:host=localhost;dbname=flattingiodb','root','');
+?>
---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+﻿app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+         .when('/', {
+             templateUrl: 'pages/account/login.html',
+             controller: 'loginController'
+        })
+        .when('/home', {
+            title:'Home',
+            templateUrl: 'pages/common/incidents.html',
+          
+        })
+});
