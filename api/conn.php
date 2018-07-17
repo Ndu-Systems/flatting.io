5f0b79043a4f6755c1d52946ed8d31bd84a3761a
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-07-17 15:46:16 +0200

    Romoved all files from ng1

--- api/conn.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-
-	//$conn = new PDO('mysql:host=127.0.0.1;dbname=ndusys0_makhawinidb','ndusys0_makhawin','xVeQFh8Rc0MfnMf');
-	$conn = new PDO('mysql:host=localhost;dbname=flattingiodb','root','');
-?>
