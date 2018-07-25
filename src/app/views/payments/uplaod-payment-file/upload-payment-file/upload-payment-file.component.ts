a1c4b8e189709b6d96aa9c1a0ffce714f51df885
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-07-25 22:44:10 +0200

    Excell can be read now !!!!

--- src/app/views/payments/uplaod-payment-file/upload-payment-file/upload-payment-file.component.ts
+++ src/app/views/payments/uplaod-payment-file/upload-payment-file/upload-payment-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'ts-xlsx';
 
 @Component({
   selector: 'app-upload-payment-file',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-payment-file.component.scss']
 })
 export class UploadPaymentFileComponent implements OnInit {
+  data: {}[];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  arrayBuffer:any;
+  file:File;
+  incomingfile(event) 
+    {
+    this.file= event.target.files[0]; 
+    }
+  
+   Upload() {
+        let fileReader = new FileReader();
+          fileReader.onload = (e) => {
+              this.arrayBuffer = fileReader.result;
+              let data = new Uint8Array(this.arrayBuffer);
+              let arr = new Array();
+              for(let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+              let bstr = arr.join("");
+              let workbook = XLSX.read(bstr, {type:"binary"});
+              let first_sheet_name = workbook.SheetNames[0];
+              let worksheet = workbook.Sheets[first_sheet_name];
+              this.data = XLSX.utils.sheet_to_json(worksheet,{raw:true});
+              console.log(this.data);
+          }
+          fileReader.readAsArrayBuffer(this.file);
+  }
 }
