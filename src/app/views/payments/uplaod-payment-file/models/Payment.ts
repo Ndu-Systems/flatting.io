f7cbbfc104a375222dda20408d6636adbbc90f09
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-07-31 20:54:01 +0200

    Mocked invoice payload

--- src/app/views/payments/uplaod-payment-file/models/Payment.ts
+++ src/app/views/payments/uplaod-payment-file/models/Payment.ts
@@ -2,3 +2,38 @@ export interface IPayment {
     Ref: number;
     Amount:number;
 }
+export interface IInvoice {
+    Ref: number;
+    Amount:number;
+    Month:string;
+    Name:string;
+    Room:string;
+}
+export interface IPaymentReport {
+    Ref: number;
+    AmountPaid:number;
+    AmountInvoiced:number;
+    Month:string;
+    Name:string;
+    Room:string;
+    Status:string;
+}
+
+export const mock_invoice:Array<IInvoice> = [
+    {
+        Ref:251,
+        Amount:2000,
+        Month:'July',
+        Name:'Ndumiso',
+        Room:'G36'
+    },
+    {
+        Ref:123,
+        Amount:4200,
+        Month:'July',
+        Name:'Themba',
+        Room:'G37'
+    }
+    
+];
+
